1f64067babe43b4802951af6d4ae0683bf8962af
David White <dwxw@users.noreply.github.com>
2025-01-30 20:53:26 +0000

    htmx indicator

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"io"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -108,6 +109,8 @@ func main() {
 
 	page := newPage()
 	e.Renderer = newTemplate()
+	e.Static("/images", "images")
+	e.Static("/css", "css")
 
 	e.GET("/", func(c echo.Context) error {
 		if err := c.Render(200, "test", page); err != nil {
@@ -139,6 +142,8 @@ func main() {
 	})
 
 	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(3 * time.Second)
+
 		idStr := c.Param("id")
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
