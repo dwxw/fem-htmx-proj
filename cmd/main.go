9c6786aecbea0db4c07095009c4d599a402ffa51
David White <dwxw@users.noreply.github.com>
2025-01-30 20:33:13 +0000

    deleting contacts

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -22,15 +23,20 @@ func newTemplate() *Templates {
 	}
 }
 
+var id = 0
+
 type Contact struct {
 	Name  string
 	Email string
+	Id    int
 }
 
 func newContact(name, email string) Contact {
+	id++
 	return Contact{
 		Name:  name,
 		Email: email,
+		Id:    id,
 	}
 }
 
@@ -38,6 +44,15 @@ type Contacts = []Contact
 
 type Data struct {
 	Contacts Contacts
+}
+
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
 }
 
 func (d *Data) hasEmail(email string) bool {
@@ -95,6 +110,9 @@ func main() {
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
+		if err := c.Render(200, "test", page); err != nil {
+			return err
+		}
 		return c.Render(200, "index", page)
 	})
 
@@ -118,6 +136,23 @@ func main() {
 			return err
 		}
 		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid id")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		return c.NoContent(200)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
